b5578ba3b1d8550007c35f06ea9da4dc29988f41
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-15 11:39:22 +0000

    Create Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<script setup>
+import MenuIcon from './icons/MenuIcon.vue';
+import Logo from './Logo.vue';
+import CartIcon from './CartIcon.vue';
+
+</script>
+
+
+<template>
+  <header class="flex justify-between m-4">
+    <div class="flex-center">
+      <button>
+        <MenuIcon />
+      </button>
+  
+      <Logo />
+    </div>
+
+    <div class="flex-center">
+      <CartIcon />
+      <img src="/assets/image-avatar.png" alt="avatar" class="h-5">
+    </div>
+  </header>
+</template>
+
+
+<style lang="postcss" scoped>
+  .flex-center{
+    @apply flex justify-between items-center space-x-4
+  }
+</style>
