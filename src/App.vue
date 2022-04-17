e9e159d6312afdb294d3cc595e7f3373e804ec34
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-17 14:22:17 +0000

    add body content

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <script setup>
 import Navbar from './components/Navbar.vue';
 import Carousel from './components/Carousel.vue';
+import PlusIcon from './components/icons/PlusIcon.vue';
+import MinusIcon from './components/icons/MinusIcon.vue';
+import CartIcon from './components/CartIcon.vue';
 
 
 </script>
@@ -11,19 +14,34 @@ import Carousel from './components/Carousel.vue';
   <main>
     <Carousel />
 
-    Sneaker Company
+    <div class="mx-4 space-y-4">
+      <h1 class="font-bold text-xs tracking-wider uppercase text-Orange mt-4">Sneaker Company</h1>
+  
+      <span class="font-bold text-2xl block text-Very-vark-blue">Fall Limited Edition Sneakers</span>
+  
+      <p class="text-Dark-grayish-blue">
+        These low-profile sneakers are your perfect casual wear companion. Featuring a
+        durable rubber outer sole, they’ll withstand everything the weather can offer.
+      </p>
+  
+      <div class="flex items-center">
+        <span class="Very-vark-blue font-bold text-2xl">$125.00</span>
 
-    Fall Limited Edition Sneakers
+        <span class="text-Orange bg-Pale-orange py-1 px-2 text-xs font-bold ml-4 rounded-sm">50%</span>
 
-    These low-profile sneakers are your perfect casual wear companion. Featuring a
-    durable rubber outer sole, they’ll withstand everything the weather can offer.
+        <span class="text-Grayish-blue line-through text-xs font-bold ml-auto">$250.00</span>
+      </div>
+  
+      <div class="bg-Light-grayish-blue rounded-lg text-Orange font-bold py-3 px-4 flex justify-between">
+        <button> <MinusIcon /> </button>
 
-    $125.00
-    50%
-    $250.00
+        <span class="text-Very-vark-blue">0</span>
 
-    0
-    Add to cart
+        <button> <PlusIcon /> </button>
+      </div>
+
+      <button class="bg-Orange text-white rounded-lg w-full py-3 px-4 flex justify-center"> <CartIcon class="fill-white mr-4" /> Add to cart</button>
+    </div>
   </main>
 
   <footer>
