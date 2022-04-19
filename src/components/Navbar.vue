b7916f8d6daf907fd3caad9738e7e4a5a5cf347c
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-19 14:41:00 +0000

    responsiveness, added cart component and included mobile menu in header

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,6 +6,7 @@ import { ref, watchEffect } from 'vue';
 import CloseIcon from './icons/CloseIcon.vue';
 import CartIcon from './icons/CartIcon.vue';
 import { useCartStore } from '../stores/cart';
+import Cart from './Cart.vue'
 
 const menu = ['Collections', 'Men', 'Women', 'About', 'Contact']
 
@@ -24,53 +25,64 @@ const cart = useCartStore()
 
 
 <template>
-  <header class="flex justify-between m-3">
+  <header class="flex justify-between m-3 md:m-0 md:mx-16 md:border-b-2">
     <div class="flex-center">
-      <button  @click="open = true">
+      <button  @click="open = true" class="md:hidden">
         <MenuIcon />
       </button>
   
-      <Logo class="mb-1" />
+      <a href="#"><Logo class="mb-1 md:mb-2 ml-4 md:ml-0 md:mr-14" /></a>
+
+      <nav class="hidden md:block space-x-6 text-Dark-grayish-blue">
+        <a href="#" v-for="item in menu" :key="item" class="px-2 inline-block py-10 hover:text-Very-vark-blue border-b-4 border-transparent hover:border-Orange transition-all duration-500 delay-75">
+          {{item}}
+        </a>
+      </nav>
     </div>
 
-    <div class="flex-center">
+    <div class="flex-center space-x-4 md:space-x-14">
       <button @click="cart.is_open = true" class="relative">
         <CartIcon />
         
         <div v-if="cart.item_quantity" class="bg-Orange text-white absolute -top-2 -right-2 py-0 px-2 text-xs rounded-full">{{cart.item_quantity}}</div>
       </button>
-      <img src="/assets/image-avatar.png" alt="avatar" class="h-8 rounded-full border-2 border-transparent hover:border-Orange">
+
+      <img src="/assets/image-avatar.png" alt="avatar" class="h-8 md:h-14 rounded-full border-2 border-transparent hover:border-Orange transition-all duration-300">
     </div>
-  </header>
 
-  <Teleport to="body">
-    <Transition name="overlay">
-      <div v-if="open" @click="open = false" class="fixed inset-0 w-full h-full flex justify-start bg-black/75">
-      </div>
-    </Transition>
-
-    <Transition>
-      <div v-if="open" class="w-1/2 bg-white h-full p-5 absolute top-0">
-        <div class="flex justify-start items-center mb-14">
-          <button @click="open = false">
-            <CloseIcon />
-          </button>
+    <!-- mobile menu -->
+    <Teleport to="body">
+      <Transition name="overlay">
+        <div v-if="open" @click="open = false" class="fixed inset-0 w-full h-full flex justify-start bg-black/75">
         </div>
+      </Transition>
 
-        <nav class="flex flex-col space-y-4 font-bold">
-          <a href="#" v-for="item in menu" :key="item">
-            {{item}}
-          </a>
-        </nav>
-      </div>
-    </Transition>
-  </Teleport>
+      <Transition>
+        <div v-if="open" class="w-1/2 bg-white h-full p-5 absolute top-0">
+          <div class="flex justify-start items-center mb-14">
+            <button @click="open = false">
+              <CloseIcon />
+            </button>
+          </div>
+
+          <nav class="flex flex-col space-y-4 font-bold">
+            <a href="#" v-for="item in menu" :key="item">
+              {{item}}
+            </a>
+          </nav>
+        </div>
+      </Transition>
+    </Teleport>
+
+    <!-- cart modal -->
+    <Cart />
+  </header>
 </template>
 
 
 <style lang="postcss" scoped>
   .flex-center{
-    @apply flex justify-between items-center space-x-4
+    @apply flex justify-between items-center
   }
 
   .v-enter-active {
