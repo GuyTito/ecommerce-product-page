e7c2801b0a63eec5cd736709f16e0e0f6e921a66
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-19 09:54:08 +0000

    hover avatar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -24,7 +24,7 @@ const cart = useCartStore()
 
 
 <template>
-  <header class="flex justify-between m-4">
+  <header class="flex justify-between m-3">
     <div class="flex-center">
       <button  @click="open = true">
         <MenuIcon />
@@ -39,7 +39,7 @@ const cart = useCartStore()
         
         <div v-if="cart.item_quantity" class="bg-Orange text-white absolute -top-2 -right-2 py-0 px-2 text-xs rounded-full">{{cart.item_quantity}}</div>
       </button>
-      <img src="/assets/image-avatar.png" alt="avatar" class="h-5">
+      <img src="/assets/image-avatar.png" alt="avatar" class="h-8 rounded-full border-2 border-transparent hover:border-Orange">
     </div>
   </header>
 
