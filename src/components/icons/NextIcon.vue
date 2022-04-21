e49d6b551e1f131830be154d78999b29b60aeff9
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-04-21 12:29:21 +0000

    made stroke color a class so that apply group-hover on it

--- src/components/icons/NextIcon.vue
+++ src/components/icons/NextIcon.vue
@@ -1,3 +1,3 @@
 <template>
-  <svg width="13" height="18" xmlns="http://www.w3.org/2000/svg"><path d="m2 1 8 8-8 8" stroke="#1D2026" stroke-width="3" fill="none" fill-rule="evenodd"/></svg>
+  <svg class="stroke-[#1D2026] group-hover:stroke-Orange" width="13" height="18" xmlns="http://www.w3.org/2000/svg"><path d="m2 1 8 8-8 8" stroke="" stroke-width="3" fill="none" fill-rule="evenodd"/></svg>
 </template>
